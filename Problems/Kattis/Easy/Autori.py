06a5b208c042a3ec24bebc0b0a9aa645c003dd0b
jpaetsch <j.paetsch@live.com>
2022-06-16 15:11:41 -0600

    Deleted all Kattis and Leetcode problems - will do generalized versions instead

--- Problems/Kattis/Easy/Autori.py
+++
@@ -1,9 +0,0 @@
-# Split input along the hyphen delimiter
-individualNames = input().split("-")
-
-# Append the first letter of each name into the output string
-outputString = ""
-for name in individualNames:
-    outputString += name[0]
-
-print(outputString)
--- Problems/Kattis/Easy/GrassSeed.py
+++
@@ -1,9 +0,0 @@
-seedCost = float(input())
-lawnNumber = int(input())
-totalCost = 0
-for i in range(lawnNumber):
-    width, length = input().split()
-    width = float(width)
-    length = float(length)
-    totalCost += width*length*seedCost
-print('%.7f'%totalCost)
